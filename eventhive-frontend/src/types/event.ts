2f1c820f8d7819fd9d48ae6da2537d0e176d07aa
Theertha Santhosh <141830859+Theertha-santhosh@users.noreply.github.com>
2025-03-10 21:19:36 +0530

    Added updated even.ts

--- eventhive-frontend/src/types/event.ts
+++ eventhive-frontend/src/types/event.ts
@@ -1,3 +1,5 @@
+// types/event.ts
+
 export interface Event {
     id: number;
     image: string;
@@ -5,4 +7,16 @@ export interface Event {
     startDate: string;
     time: string;
     type: 'Online' | 'Offline';
-}
+  }
+  
+  export interface CreateEventForm {
+    name: string;
+    startDate: string;
+    time: string;
+    type: 'Online' | 'Offline'; // This dictates the type of event
+    description?: string;   // Optional description
+    endDate?: string;       // Optional end date
+    registrationLink?: string; // Optional registration link
+    poster: File | null;    // For uploading the image
+    image: string;           // Base64 representation of the image (for preview)
+  }
